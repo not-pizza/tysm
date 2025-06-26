c8985d561773017542dd273b9f1971e449273d65
Andre Popovitch <andre@popovit.ch>
2025-06-26 01:12:26 -0500

    @anchpop/v0.10 (#31)
    * improve error messages further
    
    * update schemars version
    
    * update schemars

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 //!
 //! This library uses the [schemars](https://docs.rs/schemars/latest/schemars/index.html) crate to generate a schema for the desired response type. It also uses [serde](https://docs.rs/serde/latest/serde/index.html) to deserialize the response into the desired type. Install these crates like so:
 //!
-//! `cargo add tysm serde schemars@1.0.0-alpha.17`.
+//! `cargo add tysm serde schemars`.
 //!
 //! ## Usage
 //!
